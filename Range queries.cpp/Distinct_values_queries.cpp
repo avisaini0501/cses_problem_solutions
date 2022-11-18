77226255dd5e8c3607a75d16638eafaf412974f1
Avi_saini <81501977+avisaini0501@users.noreply.github.com>
2022-11-18 19:37:44 +0530

    Create Distinct_values_queries.cpp

---
+++ Range queries.cpp/Distinct_values_queries.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define int long long
+#define endl '\n'
+#define mod 1000000007
+#define pb push_back
+#define pii pair<int , int>
+#define vc vector
+#define vp vector<pair<int , int>>
+#define vi vector<int>
+#define vii vector<vector<<int>>
+#define loop(a , b) for(int i = a; i < b; i++)
+#define rep(a , b) for(int i = a; i <= b; i++)
+#define all(x) x.begin() , x.end()
+#define triplet pair<int , pair<int , int>>
+#define goog(tno) cout<<"Case #"<<tno<<": "
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T> using ordered_set = tree<T, null_type, less<T> , rb_tree_tag, tree_order_statistics_node_update>;
+
+vi adj[200005];
+int dp[200005];
+int ans = 0 , x = 0;
+
+void dfs(int s , int p , int dep){
+    for(auto c : adj[s]){
+        if(c != p){
+            dfs(c , s , dep+1);
+        }
+    }
+    if(dep > ans){
+        ans = dep;
+       x = s;
+    }
+}
+
+int32_t main(){
+    fast_io;
+    int n;
+    cin>>n;
+    loop(0 , n - 1){
+        int a , b;
+        cin>>a>>b;
+        adj[a].pb(b);
+        adj[b].pb(a);
+    }
+    dfs(1 , 0 , 0);
+    dfs(x , 0 , 0);
+    cout<<ans;
+   
+}
