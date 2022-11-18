22285fece8aa3798dd4c2bf0747dd8ef796f7194
Avi_saini <81501977+avisaini0501@users.noreply.github.com>
2022-11-18 19:39:14 +0530

    Update Distinct_values_queries.cpp

--- Range queries.cpp/Distinct_values_queries.cpp
+++ Range queries.cpp/Distinct_values_queries.cpp
@@ -1,53 +1,89 @@
 #include<bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
-#define int long long
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
 #define endl '\n'
 #define mod 1000000007
 #define pb push_back
-#define pii pair<int , int>
+#define pii pair<ll , ll>
 #define vc vector
-#define vp vector<pair<int , int>>
-#define vi vector<int>
-#define vii vector<vector<<int>>
-#define loop(a , b) for(int i = a; i < b; i++)
-#define rep(a , b) for(int i = a; i <= b; i++)
+#define vp vector<pair<ll , ll>>
+#define vi vector<ll>
+#define vii vector<vector<<ll>>
+#define loop(a , b) for(ll i = a; i < b; i++)
+#define rep(a , b) for(ll i = a; i <= b; i++)
 #define all(x) x.begin() , x.end()
-#define triplet pair<int , pair<int , int>>
+#define triplet pair<ll , pair<ll , ll>>
 #define goog(tno) cout<<"Case #"<<tno<<": "
 #define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
 using namespace std;
-using namespace __gnu_pbds;
-template <typename T> using ordered_set = tree<T, null_type, less<T> , rb_tree_tag, tree_order_statistics_node_update>;
+/*using namespace __gnu_pbds; */ 
+/*template <typename T> using ordered_set = tree<T, null_type, less<T> , rb_tree_tag, tree_order_statistics_node_update>;*/
 
-vi adj[200005];
-int dp[200005];
-int ans = 0 , x = 0;
+struct BIT {
+	int size;
+	vector<int> bit;
+	BIT(int n) : size(n), bit(n + 1) {}
+	void update(int x, int v) {
+		x++;
+		for (; x <= size; x += x & (-x)) {
+			bit[x] += v;
+		}
+	}
+    
+	int query(int b){
+		b++;
+		int result = 0;
+		for (; b > 0; b -= b & (-b)) {
+			result += bit[b];
+		}
+		return result;
+	}
+};
 
-void dfs(int s , int p , int dep){
-    for(auto c : adj[s]){
-        if(c != p){
-            dfs(c , s , dep+1);
-        }
-    }
-    if(dep > ans){
-        ans = dep;
-       x = s;
-    }
-}
+int main() {
+	int n, q;
+	cin >> n >> q;
 
-int32_t main(){
-    fast_io;
-    int n;
-    cin>>n;
-    loop(0 , n - 1){
-        int a , b;
-        cin>>a>>b;
-        adj[a].pb(b);
-        adj[b].pb(a);
-    }
-    dfs(1 , 0 , 0);
-    dfs(x , 0 , 0);
-    cout<<ans;
-   
+	vector<int> arr(n);
+	vector<vector<pair<int, int>>> queries(n);
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	for (int i = 0; i < q; i++) {
+		int a, b;
+		cin >> a >> b;
+		a--, b--;
+		queries[a].push_back({b, i});
+	}
+
+	BIT bit(n);
+	// last_index[val] is the left-most index where arr[last_index[val]] = val.
+	map<int, int> last_index;
+	vector<int> solution(q, -1);
+	// Update the indices and answer queries from right to left.
+	for (int i = n - 1; i >= 0; i--) {
+		int val = arr[i];
+		/*
+		 * If val already appeared earlier, then the saved value is no
+		 * longer the left-most index, so erase it from our BIT.
+		 */
+		if (last_index.count(val) > 0) bit.update(last_index[val], -1);
+		// i becomes the left-most index.
+		last_index[val] = i;
+		bit.update(i, 1);
+
+		// Answer all queires with a == i.
+		for (auto &qr : queries[i]) {
+			/*
+			 * The solution for this query is bit[i,b].
+			 * This equals bit[0,b] since bit[0,i-1] = 0.
+			 */
+			solution[qr.second] = bit.query(qr.first);
+		}
+	}
+
+	for (auto &a : solution) {
+		cout << a << "\n";
+	}
 }
