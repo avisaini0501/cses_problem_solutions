8d5b3e4bad5fdbd2c377762e0ccb27f525affa45
Avi_saini <81501977+avisaini0501@users.noreply.github.com>
2022-11-20 19:08:46 +0530

    Add files via upload

---
+++ Tree algorithm/Tree_matching.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define int long long
+#define endl '\n'
+#define mod 1000000007
+#define pb push_back
+#define pii pair<int , int>
+#define vc vector
+#define vp vector<pair<int , int>>
+#define vi vector<int>
+#define vii vector<vector<<int>>
+#define loop(a , b) for(int i = a; i < b; i++)
+#define rep(a , b) for(int i = a; i <= b; i++)
+#define all(x) x.begin() , x.end()
+#define triplet pair<int , pair<int , int>>
+#define goog(tno) cout<<"Case #"<<tno<<": "
+#define fast_io ios_base::sync_with_stdio(false);cin.tie(NULL)
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T> using ordered_set = tree<T, null_type, less<T> , rb_tree_tag, tree_order_statistics_node_update>;
+
+
+vi adj[200005];
+bool vis[200005];
+int ans = 0;
+
+void dfs(int s , int p){
+    for(auto c : adj[s]){
+        if(c != p){
+            dfs(c , s);
+            if(!vis[c] && !vis[s]){
+                ans++;
+                vis[c] = 1;
+                vis[s] = 1;
+            }
+        }
+    }
+}
+
+int32_t main(){
+    fast_io;
+    int n;
+    cin>>n;
+    loop(0 , n - 1){
+        int a , b;
+        cin>>a>>b;
+        adj[a].pb(b);
+        adj[b].pb(a);
+    }
+    dfs(1 , 0);
+    cout<<ans;
+}
